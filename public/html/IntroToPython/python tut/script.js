5ddcd09f0a6920b84e1dcb620169b5587d71aefa
1E0-cpu <leo.rosnav@gmail.com>
2024-12-12 09:05:57 +0100

    update script.js

--- public/html/IntroToPython/python tut/script.js
+++ public/html/IntroToPython/python tut/script.js
@@ -1,8 +1,8 @@
 function prepareMain() {
     var main = document.getElementById('main')
-    main.innerHTML += '<div class="toggle" onclick="change_theme()"><div class="toggleIndicator"></div></div>'
-    main.innerHTML += '<div id="menuButton" onclick="changeSidenavState()"><div></div><div></div><div></div></div>'
-    main.innerHTML += '<h1 id="title">Basic Introduction to Python</h1>'
+    main.innerHTML += '\n<div class="toggle" onclick="change_theme()"><div class="toggleIndicator"></div></div>'
+    main.innerHTML += '\n<div id="menuButton" onclick="changeSidenavState()"><div></div><div></div><div></div></div>'
+    main.innerHTML += '\n<h1 id="title">Basic Introduction to Python</h1>'
 
 }
 
